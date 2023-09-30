1c419fbf62edbab8fc33759dfbf9712b2ea566e8
pantuman116 <inami.yuu.yuu0@gmail.com>
2023-10-01 07:52:31 +0900

    Googleアナリディクス対応、タイトル修正

--- src/views/pages/Layout.php
+++ src/views/pages/Layout.php
@@ -8,11 +8,27 @@
     <title><?php echo $title; ?></title>
 </head>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-95Q1P53EVE"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-95Q1P53EVE');
+</script>
+
 <body>
     <header class="navbar shadow-sm p-3 mb-5 bg-white">
-        <h1 class="h2">
-            <a class="text-body text-decoration-none" href="/">Seasoning Converter</a>
-        </h1>
+        <div class="container">
+            <a class="h3 text-body text-decoration-none" href="/">Seasoning Converter</a>
+        </div>
+        <div class="container">
+            <a class="h6 text-body text-decoration-none" href="/">〜調味料別体積重量変換器〜</a>
+        </div>
     </header>
     <div class="container">
         <?php include $contents; ?>
